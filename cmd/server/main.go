da09cce3d2350b5e817c862bc23e36a19b4651f0
olezhek28 <olezhek28.courses@gmail.com>
2025-10-19 13:47:37 +0300

    add two http client

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,6 +11,8 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-co-op/gocron/v2"
+	"github.com/olezhek28/wether-service/internal/client/http/geocoding"
+	"github.com/olezhek28/wether-service/internal/client/http/open_meteo"
 )
 
 const httpPort = ":3000"
@@ -18,12 +21,35 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
+	httpClient := &http.Client{
+		Timeout: time.Second * 10,
+	}
+	geocodingClient := geocoding.NewClient(httpClient)
+	openMeteoClient := open_meteo.NewClient(httpClient)
+
 	r.Get("/{city}", func(w http.ResponseWriter, r *http.Request) {
 		city := chi.URLParam(r, "city")
 
 		fmt.Printf("Requested city: %s\n", city)
 
-		_, err := w.Write([]byte("привет!"))
+		geoRes, err := geocodingClient.GetCoords(city)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		openMetRes, err := openMeteoClient.GetTemperature(geoRes.Latitude, geoRes.Longitude)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		raw, err := json.Marshal(openMetRes)
+		if err != nil {
+			log.Println(err)
+		}
+
+		_, err = w.Write(raw)
 		if err != nil {
 			log.Println(err)
 		}
